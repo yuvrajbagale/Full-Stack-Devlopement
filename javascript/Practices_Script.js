3cc6bb37890b48d851ab6be2c85c63c066921515
yuvrajbagale <yuvrajbagale123@gmail.com>
2024-03-23 15:10:58 +0530

    Update

---
+++ javascript/Practices_Script.js
@@ -0,0 +1,105 @@
+// let firstname = "yuvraj";
+// let lastname = "bagale";
+// const mobileno = 9763000000;
+// console.log("my frist name is" + firstname);
+// console.log("my last Name is", lastname);
+// console.log(mobileno, firstname, lastname);
+
+// tempate literal
+
+// console.log(
+//   `My Current frist name is ${firstname} and my last name is ${lastname} is and my modile number is ${mobileno}`
+// );
+
+// Histroy
+// bascic piller of an programming languages
+// values
+// data types
+// diffrences between primitive and non premitives datatypes
+// console.log => how to print something on terminal
+//Decalring variables
+//Tempate literal
+
+//Operators
+
+//Assigment operators
+let x = 10;
+let y = "10";
+// arithmatics opreators
+// + - *  / % => remainders
+
+// let z = x + y;
+// console.log(`result: ${z}`);
+
+// == or ===
+// != or !==
+// > >= < <=
+// || (OR)
+// && (AND)
+
+// basics of Programming languages
+// values operations variables
+// decisions loops functions
+
+// console.log(x === y);
+
+// let age = 17;
+
+// if (age >= 18) {
+//   console.log("you are allowed inside the park");
+// } else {
+//   console.log("out of the park");
+// }
+
+// let gender = "female";
+
+// if (gender == "female") {
+//   console.log("Yes u are allowd");
+// } else {
+//   console.log("not allowed");
+// }
+
+// let standard = 3;
+
+// if (standard == 1) {
+//   console.log("Room First");
+// } else if (standard == 2) {
+//   console.log("Room Second");
+// } else if (standard == 3) {
+//   console.log("Room Three");
+// } else {
+//   console.log("You are Principal");
+// }
+
+// let rating =5;
+// if(rating==5){
+//     console.log('5 Stars');
+// } else if(rating==4);{
+//     console.log('4 Stars');
+// } else if(rating==3){
+//     console.log('3 Stars');
+// }else{
+//     console.log('No Rating')
+// }
+
+// switch case
+
+// let user = "Admin";
+// switch (user) {
+//   case "Admin":
+//     console.log("Access Granted!");
+//     break;
+//   case "Mentor":
+//     console.log("Hello Mentor");
+//     break;
+
+//   default:
+//     console.log("Hello User!");
+// }
+
+//Array
+
+// let name = ["A", "B", "C", "D", "E", "F", "G"];
+
+// console.log(name);
+// console.log(name[6]);
